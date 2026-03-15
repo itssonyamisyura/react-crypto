d714146ca26ceb98ea6454e03871c68f5b37c890
Sofia <itssonyamisyura@gmail.com>
2026-03-15 18:52:34 +0100

    implement React form

--- src/context/crypto-context.jsx
+++ src/context/crypto-context.jsx
@@ -15,32 +15,45 @@ export function CryptoContextProvider({children}) {
     const [crypto, setCrypto] = useState([]);
     const [assets, setAssets] = useState([]);
 
+    function mapAssets(assets, result) {
+        return assets.map(asset => {
+            const coin = result.find((c) => c.id === asset.id)
+
+            //asset price => we bought
+            //coin price => actual(market) price
+            return {
+                grow: asset.price < coin.price, // grows
+                growPercent: percentDifference(asset.price, coin.price),
+                totalAmount: asset.amount * coin.price,
+                totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+                ...asset,
+            }
+        })
+    }
+
     useEffect(() => {
         async function preload() {
             setLoading(true);
             const {result} = await fakeFetchCrypto();
             const assets = await fetchAssets();
 
-            setAssets(assets.map(asset => {
-                const coin = result.find((c) => c.id === asset.id)
-
-                //asset price => we bought
-                //coin price => actual(market) price
-                return {
-                    grow: asset.price < coin.price, // grows
-                    growPercent: percentDifference(asset.price, coin.price),
-                    totalAmount: asset.amount * coin.price,
-                    totalProfit: asset.amount * coin.price - asset.amount * asset.price,
-                    ...asset,
-                }
-            }));
+            setAssets(
+                (mapAssets(assets, result)) 
+            );
             setCrypto(result);
             setLoading(false);
         }
         preload();
     }, [])
 
-    return <CryptoContext.Provider value={{loading, crypto, assets}}>{children}</CryptoContext.Provider>
+    function addAsset(newAsset) {
+        setAssets((prev) => mapAssets([...prev, newAsset], crypto)) //change state->add new asset
+    }
+
+    return (
+    <CryptoContext.Provider value={{loading, crypto, assets, addAsset}}>
+        {children}
+    </CryptoContext.Provider>)
 }
 
 export default CryptoContext;
